df9f9b0d8b8ee360ef37d6a94dc838612c167047
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-24 01:14:41 +0800

    add problems

--- src/index.ts
+++ src/index.ts
@@ -14,38 +14,7 @@ export function writeJSON(filepath: string, obj: any) {
 }
 
 function readJSONFile<T>(filepath: string) {
-  return JSON.parse(fs.readFileSync(`query/${filepath}`, "utf8")) as T;
-}
-
-class Chapter extends Array<{ isbn_c_p: string; link: string }> {}
-
-class Book extends Map<string, Chapter> {
-  isbn: string;
-  name: string;
-  constructor(isbn: string) {
-    super();
-    this.isbn = isbn;
-    const name = booklist.find((b) => b.isbn === isbn)?.name;
-    if (!name) throw new Error("Book not found");
-    this.name = name;
-  }
-  get chapters() {
-    return [...this.keys()].sort();
-  }
-  addProblem(prob: { isbn_c_p: string; link: string }) {
-    const [isbn, c, p] = prob.isbn_c_p.split("_");
-    if (!this.has(c)) this.set(c, new Chapter());
-    this.get(c)!.push(prob);
-  }
-}
-
-const booklist = readJSONFile("books.json") as { name: string; isbn: string }[];
-
-const books = new Map<string, Book>();
-function addProblem(prob: { isbn_c_p: string; link: string }) {
-  const [isbn, c, p] = prob.isbn_c_p.split("_");
-  if (!books.has(isbn)) books.set(isbn, new Book(isbn));
-  books.get(isbn)!.addProblem(prob);
+  return JSON.parse(fs.readFileSync(`${filepath}`, "utf8")) as T;
 }
 
 (async () => {
