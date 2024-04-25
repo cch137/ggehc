79b57d13dd99f83011e1683e94225cd283065d18
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-25 18:13:20 +0800

    feat: add cached prop

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import fs from "fs";
 import path from "path";
 
 function writeFile(filepath: string, content: string) {
-  filepath = `query/${filepath}`;
   try {
     fs.mkdirSync(path.dirname(filepath), { recursive: true });
   } catch {}
   return fs.writeFileSync(filepath, content);
 }
 
-export function writeJSON(filepath: string, obj: any) {
+export function writeJSONFile(filepath: string, obj: any) {
   return writeFile(filepath, JSON.stringify(obj, null, 0));
 }
 
@@ -17,8 +16,9 @@ function readJSONFile<T>(filepath: string) {
   return JSON.parse(fs.readFileSync(`${filepath}`, "utf8")) as T;
 }
 
+type Problem = { isbn_c_p: string; link: string };
+
 (async () => {
-  const problems: { isbn_c_p: string; link: string }[] =
-    readJSONFile("problems.json");
+  const books = fs.readdirSync("query/books/");
   console.log("done");
 })();
