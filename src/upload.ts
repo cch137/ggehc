d7883d193e76d9e3c6a49fc35ab6b05acf3932bb
cch137 <96408836+cch137@users.noreply.github.com>
2024-04-27 16:31:27 +0800

    upload

--- src/upload.ts
+++ src/upload.ts
@@ -2,13 +2,19 @@ import { execSync } from "child_process";
 
 (async () => {
   const push = () => {
+    console.log("START...");
+    console.time("ADDED");
     execSync("git add .");
+    console.timeEnd("ADDED");
+    console.time("COMMITED");
     execSync('git commit -m "upload"');
+    console.timeEnd("COMMITED");
+    console.time("PUSHED");
     execSync("git push");
-    console.log("PUSHED");
-    setTimeout(push, 10000);
+    console.timeEnd("PUSHED");
+    setTimeout(push, 0);
   };
-  setTimeout(push, 10000);
+  setTimeout(push);
 })();
 
 // CMD: node ./dist/upload.js
